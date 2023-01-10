4da9418e2b31930f661cdc7d0de0bbf2fa8910f6
Lpeyriguer <94975575+Alexlu94350@users.noreply.github.com>
2023-01-10 23:37:17 +0100

    Update pageWeb.js

--- pageWeb.js
+++ pageWeb.js
@@ -1,15 +1,38 @@
-// Création d'un titre pour la page
-var title = document.createElement("h1");
-title.innerHTML = "Le petit coin";
-document.body.appendChild(title);
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>FONT</title>
 
-// Ajout d'un paragraphe de texte
-var text = document.createElement("p");
-text.innerHTML = "Mon texte";
-document.body.appendChild(text);
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<nav class="bloc"></nav>
+<nav>
+    <div class="container">
+        <img src="logo.jpg">    
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="index2.html">Contact</a></li>
+            <p>Accueil</p>
+        </ul>
+    </div>
+</nav>
 
-// Ajout d'un lien
-var link = document.createElement("a");
-link.href = "https://www.google.com";
-link.innerHTML = "Google";
-document.body.appendChild(link);
+<header>
+    <h1>Accueil</h1>
+    
+</header>
+
+
+<p class="text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+</body>
+</html>
