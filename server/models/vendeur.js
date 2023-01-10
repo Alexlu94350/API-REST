ff303cb6f15d7f12f6c3803353bab128dd9d63d3
ValentinCl <122225576+ValentinCl@users.noreply.github.com>
2023-01-10 22:49:10 +0100

    Update vendeur.js

--- server/models/vendeur.js
+++ server/models/vendeur.js
@@ -1,81 +1,74 @@
-// Import des modules necessaires
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
 // Connexion à la base de donnée
-const connection = mysql.createConnection({
-    host: 'hostname',
-    user: 'username',
-    password: 'password',
-    database: 'database_name'
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'hostname',
+  dialect: 'mysql'
 });
-connection.connect();
-console.log("Connected to the MYSQL database!");
 
-// Utilisation du body-parser pour lire les données en json
+//Définir le modèle de donnée pour les vendeurs
+const Seller = sequelize.define('seller', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
 app.use(bodyParser.json());
 
-// Enregistrement des routes pour les vendeurs
-app.route('/sellers')
-    // Récupère tous les vendeurs
-    .get((req, res) => {
-        connection.query(`SELECT * FROM sellers`, (err, rows, fields) => {
-            if (!err) {
-                res.send(rows);
-            } else {
-                console.log(err);
-            }
-        });
-    })
-    // Ajoute un nouveau vendeur
-    .post((req, res) => {
-        let seller = req.body;
-        let sql = `INSERT INTO sellers (name, email) VALUES ('${seller.name}', '${seller.email}')`;
-        connection.query(sql, (err, result) => {
-            if (!err) {
-                res.send(result);
-            } else {
-                console.log(err);
-            }
-        });
-    });
+// route pour récupérer tous les vendeurs
+app.get('/sellers', (req, res) => {
+  Seller.findAll().then(sellers => {
+    res.send(sellers);
+  });
+});
 
-// Enregistrement de route pour un vendeur spécifique
-app.route('/sellers/:id')
-    // Récupère un vendeur spécifique
-    .get((req, res) => {
-        connection.query(`SELECT * FROM sellers WHERE id = ${req.params.id}`, (err, rows, fields) => {
-            if (!err) {
-                res.send(rows);
-            } else {
-                console.log(err);
-            }
-        });
-    })
-    // Met à jour un vendeur spécifique
-    .put((req, res) => {
-        let seller = req.body;
-        let sql = `UPDATE sellers SET name = '${seller.name}', email = '${seller.email}' WHERE id = ${req.params.id}`;
-        connection.query(sql, (err, result) => {
-            if (!err) {
-                res.send(result);
-            } else {
-                console.log(err);
-            }
-        });
-    })
-    // Supprime un vendeur spécifique
-    .delete((req, res) => {
-        connection.query(`DELETE FROM sellers WHERE id = ${req.params.id}`, (err, result) => {
-            if (!err) {
-                res.send(result);
-            } else {
-                console.log(err);
-            }
-        });
-    });
+// route pour ajouter un vendeur
+app.post('/seller', (req, res) => {
+  const seller = req.body;
+  Seller.create(seller).then(() => {
+    res.send(seller);
+  }).catch(err => {
+    console.log(err);
+  });
+});
 
-// Ecoute sur un port spécifique
-app
+// route pour mettre à jour un vendeur
+app.put('/seller/:id', (req, res) => {
+  const seller = req.body;
+  Seller.update(seller, {
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    res.send(seller);
+  }).catch(err => {
+    console.log(err);
+  });
+});
+
+// route pour supprimer un vendeur
+app.delete('/seller/:id', (req, res) => {
+  Seller.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    res.send({message: "Seller deleted"});
+  }).catch(err => {
+    console.log(err);
+  });
+});
+
+//Démarrage du serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
