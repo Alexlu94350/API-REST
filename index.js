0259c43fdc705a2d97074ffbf68e8ec6bafd5c4e
ValentinCl <122225576+ValentinCl@users.noreply.github.com>
2023-01-10 15:18:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 const express = require("express");
 const userRouter = require("./routes/users");
-const securityRouter = require("./routes/security");
-const helloRouter = require("./routes/hello");
-const checkRequestFormat = require("./middlewares/checkRequestFormat");
-const errorHandler = require("./middlewares/errorHandler");
+
 const app = express();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-app.use(function monitor(req, res, next) {
-  // Process request monitoring
-  console.log("request", req.url);
-
-  // Override default functions with monitored ones
-  const jsonFnc = res.json;
-  res.json = function (...args) {
-    console.log("response", res.statusCode, JSON.stringify(args));
-    return jsonFnc.call(res, ...args);
-  };
-  const sendStatusFnc = res.sendStatus;
-  res.sendStatus = function (...args) {
-    const result = sendStatusFnc.call(res, ...args);
-    console.log("response", res.statusCode);
-    return result;
-  };
-
-  next();
+/**
+ * Exemple Hello
+ */
+app.get("/hello", (request, response) => {
+  response.send("coucou");
 });
-
-app.use(checkRequestFormat);
-
-app.use(helloRouter);
-app.use(securityRouter);
+app.post("/hello", (req, res) => res.send("POST coucou"));
+app.put("/hello/:id", (req, res) => res.send("PUT coucou " + req.params.id));
+app.delete("/hello/:titi", (req, res) =>
+  res.send("DELETE coucou " + req.params.titi)
+);
+app.use(express.json());
 app.use(userRouter);
 
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server is listening on port " + PORT));
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
