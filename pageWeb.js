91d5ff788e978996438fc74f1f5558c11ab532a4
ValentinCl <122225576+ValentinCl@users.noreply.github.com>
2023-01-10 22:30:00 +0100

    Update pageWeb.js

--- pageWeb.js
+++ pageWeb.js
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <script>
+    function getData() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("users").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "get_users.php", true);
+      xhttp.send();
+    }
+  </script>
+</head>
+<body onload="getData()">
+  <table id="users"></table>
+</body>
+</html>
